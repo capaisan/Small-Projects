f0fe92a482e7c3962a5237e11adf579961ce807f
capaisan <77017295+capaisan@users.noreply.github.com>
2022-06-24 18:08:06 -0400

    Delete project1.h

--- project1.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-void map(int map_size, int treasure_location_x, int treasure_location_y);
-
-int options(int selection, int &treasure_location_x, int &treasure_location_y);
-
-bool win(int x, int y, bool winning);
